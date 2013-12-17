09f1cac11620b1a0f5a6999bcc95ed1980492973
eiriktsarpalis <eirik.tsarpalis@gmail.com>
2013-12-17 18:28:23 +0200

    add standalone implementation

---
+++ Nessos.CustomFsi.Standalone/MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using Nessos.CustomFsi.Lib;
+using Microsoft.Win32;
+
+namespace StandalonePlugin
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Apply
+            this.SetRegistryValues();
+        }
+
+        private void SetRegistryValues()
+        {
+            this.FsiPath.Text = RegistryResolver.CustomFsiPath;
+            this.FsiEnabled.IsChecked = RegistryResolver.PluginEnabled;
+        }
+
+        private void commit(object sender, RoutedEventArgs e)
+        {
+            // apply changes
+            RegistryResolver.SetConfig((bool)this.FsiEnabled.IsChecked, this.FsiPath.Text);
+
+            base.Close();
+        }
+
+        private void cancel(object sender, RoutedEventArgs e)
+        {
+            // revert changes
+            this.SetRegistryValues();
+
+            base.Close();
+        }
+
+        private void browse(object sender, RoutedEventArgs e)
+        {
+            // Create an instance of the open file dialog box.
+            Microsoft.Win32.OpenFileDialog openFileDialog = new Microsoft.Win32.OpenFileDialog();
+
+            // Set filter options and filter index.
+            openFileDialog.Filter = "Executable Files (.exe)|*.exe|All Files (*.*)|*.*";
+            openFileDialog.FilterIndex = 1;
+
+            // set initial directory
+            if (System.IO.File.Exists(this.FsiPath.Text))
+            {
+                string currentPath = System.IO.Path.GetDirectoryName(this.FsiPath.Text);
+                openFileDialog.InitialDirectory = currentPath;
+            }
+
+            // Call the ShowDialog method to show the dialog box.
+            bool? userClickedOK = openFileDialog.ShowDialog();
+
+            // Process input if the user clicked OK.
+            if (userClickedOK == true)
+            {
+                // Read the first line from the file and write it the textbox.
+                FsiPath.Text = openFileDialog.FileName;
+            }
+        }
+    }
+}
